5a3deeeb2d79e1efd56caca0fc900fbe065348a4
金龙熙 <308561217@qq.com>
2016-12-22 09:55:07 +0800

    修复一个漏洞

--- personErp-background/hot-deploy/personerp/src/com/banfftech/personerp/PersonErpQueryService.java
+++ personErp-background/hot-deploy/personerp/src/com/banfftech/personerp/PersonErpQueryService.java
@@ -3,30 +3,30 @@ package com.banfftech.personerp;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
-import org.apache.batik.util.EventDispatcher.Dispatcher;
+
 import org.ofbiz.base.util.UtilMisc;
 import org.ofbiz.entity.Delegator;
 import org.ofbiz.entity.GenericEntityException;
 import org.ofbiz.entity.GenericValue;
-import org.ofbiz.entity.condition.EntityCondition;
 import org.ofbiz.service.DispatchContext;
 import org.ofbiz.service.LocalDispatcher;
 import org.ofbiz.service.ServiceUtil;
 
+import javolution.util.FastList;
+
 public class PersonErpQueryService {
 	public static final String module = PersonErpQueryService.class.getName();
 	
-	 /**
+	/**
 	 * 查询用户信息
 	 * @param dctx
 	 * @param context
 	 * @return Map
 	 */
-	public static Map<String,Object> findPersonInfo(DispatchContext dctx,Map<String,Object> context){
+	public static Map<String, Object> findPersonInfo(DispatchContext dctx, Map<String, Object> context) {
 		LocalDispatcher dispatcher = dctx.getDispatcher();
 		Delegator delegator = dispatcher.getDelegator();
-		String partyId =(String)context.get("partyId");
-		
+		String partyId = (String) context.get("partyId");
 		GenericValue personInfo=null;
 		try {
 			personInfo = delegator.findByPrimaryKeyCache("PartyAndPerson", UtilMisc.toMap("partyId", partyId));
@@ -34,10 +34,16 @@ public class PersonErpQueryService {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+		List<String> personInfoList = FastList.newInstance();
+		String firstName= personInfo.get("firstName").toString();
+		String lastName= personInfo.get("lastName").toString();
+		String gender= personInfo.get("gender").toString();
+		personInfoList.add(firstName);
+		personInfoList.add(lastName);
+		personInfoList.add(gender);
 		Map<String, Object> result = ServiceUtil.returnSuccess();
-		result.put("personInfo", personInfo);
+		result.put("personInfoList", personInfoList);
 		return result;
-	}
 
+	}
 }
